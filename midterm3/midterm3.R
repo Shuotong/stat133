f1b22cdf1c8bea1da5482b1e63c081c3d6c4626b
GamaWu <spritewu@berkeley.edu>
2015-04-17 13:54:48 -0700

    Midterm 3 DONE

--- midterm3/midterm3.R
+++ midterm3/midterm3.R
@@ -8,7 +8,11 @@
 # and return the following
 #   <num.brac>: an integer indicating how many elements of <chvec> contain the "["
 #     symbol. For example: numBracElements(c('digit', '[:digit:]', '[]')) should return 2
-
+numBracElements = function(chvec){
+  number = grep('\\[', chvec)
+  num.brac = length(number)
+  return(num.brac)
+}
 
 
 
@@ -19,7 +23,21 @@
 #
 # and return the following
 #   <total>: A single number (the maximum of all digits in chvec)
-
+maxDigits = function(chvec){
+  temp = strsplit(chvec, split= "")
+  digits =grep("[0-9]", temp[[1]])
+  if (length(digits) == 0){
+    return(0)
+  }
+  maxD = -1
+  for(i in 1:length(digits)){
+    cur = temp[[1]][digits[i]]
+    if(cur > maxD){
+      maxD = cur
+    }
+  }
+  return(maxD)
+}
 
 
 # Some test cases:
@@ -30,13 +48,21 @@
 
 # Write a function called hisToHer that converts every instance of 
 # him in a string to her; every instance of he to she and every instance 
-# of his to hers. You can assume everything is lower case. Be careful not 
+# of his to her. You can assume everything is lower case. Be careful not 
 # to replace words that contain him/he/his (eg you don't want to
 # replace the with ther). Your function should take the argument
 #   <chvec>: A character vector
 #
 # and return
 #   <herchvec>: The same character vector with the required substitutions.
+hisToHer=function(chvec){
+  temp= strsplit(chvec, split=" ")
+  herchvec=gsub("^him$", "her", temp[[1]])
+  herchvec=gsub("^he$", "she", herchvec)
+  herchvec=gsub("^his$", "her", herchvec)
+  herchvec=paste(herchvec, collapse=" ")
+  return (herchvec)
+}
 
 
 # A test case
@@ -58,4 +84,17 @@ all.equal(
 #  <letter> The most common letter or letters in the string.
 # For example mostCommonLetter("aabbccccdddd") should return 
 # [1] "c" "d"
-
+mostCommonLetter = function(chvec){
+  lchvec = tolower(chvec)
+  rchvec = gsub("[^a-z]","",lchvec)
+  schvec = strsplit(rchvec, "")
+  t = table(schvec)
+  maxN = max(t)
+  letter = NULL
+  for(i in 1:length(t)){
+    if(t[i] == maxN){
+      letter = c(letter, names(t[i]))
+    }
+  }
+  return(letter)
+}
