7fef00a8707f8995102146e4f79906b828ad0dac
GamaWu <spritewu@berkeley.edu>
2015-04-24 22:34:06 -0700

    HW8 DONE

--- hw8/hw8.r
+++ hw8/hw8.r
@@ -18,8 +18,15 @@ genBootY = function(x, y, rep = TRUE){
   ### Return a vector of random y values the same length as y
   ### You can assume that the xs are sorted
   ### Hint use tapply here!
-  
-
+  if(length(x) != length(y)){
+    stop("length of x is not equal to length of y")
+  }
+  ys = tapply(y,x,function(t) t)
+  result = c()
+  for(i in 1:length(ys)){
+    result = c(result, sample(ys[[i]], size = 10, replace = TRUE))
+  }
+  return(result)
 }
 
 genBootR = function(fit, err, rep = TRUE){
@@ -27,7 +34,12 @@ genBootR = function(fit, err, rep = TRUE){
   ### Add the errors to the fit to create a y vector
   ### Return a vector of y values the same length as fit
   ### HINT: It can be easier to sample the indices than the values
-  
+  errors = sample(err, size = length(fit), replace = FALSE)
+  result = c()
+  for(i in 1:length(fit)){
+    result = c(result, fit[i] + errors[i])
+  }
+  return(result)
  
 }
 
@@ -37,8 +49,13 @@ fitModel = function(x, y, degree = 1){
   ### y and x are numeric vectors of the same length
   ### Return the coefficients as a vector 
   ### HINT: Take a look at the repBoot function to see how to use lm()
-  
- 
+  if(degree == 1){
+    coe = lm(y ~ x)
+  }
+  if(degree == 2){
+    coe = lm(y ~ x + I(x^2))
+  }
+  coeff = as.numeric(coe$coefficients)
   return(coeff)
 }
 
@@ -46,17 +63,29 @@ oneBoot = function(data, fit = NULL, degree = 1){
   ###  data are either your data (from call to getData)
   ###  OR fit and errors from fit of line to data
   ###  OR fit and errors from fit of quadratic to data  
-
- 
+  if(is.null(fit)){
+    ynew = genBootY(data[[1]],data[[2]])
+  }else{
+    ynew = genBootR(fit[[1]],fit[[2]])
+  }
   ### Use fitModel to fit a model to this bootstrap Y 
- 
+  return(fitModel(data$x, ynew, degree))
 }
 
 repBoot = function(data, B = 1000){
   
   ### Set up the inputs you need for oneBoot, i.e.,
   ### create errors and fits for line and quadratic
-
+  x = data[[1]]
+  y = data[[2]]
+  d1model = lm(y ~ x)
+  fitted1 = as.numeric(d1model$fitted.values)
+  err1 = as.numeric(d1model$residuals)
+  fit1 = data.frame(fitted1,err1)
+  d2model = lm(y ~ x + I(x^2))
+  fitted2 = as.numeric(d2model$fitted.values)
+  err2 = as.numeric(d2model$residuals)
+  fit2 = data.frame(fitted2,err2)
   ### replicate a call to oneBoot B times
   ### format the return value so that you have a list of
   ### length 4, one for each set of coefficients
@@ -75,7 +104,30 @@ repBoot = function(data, B = 1000){
   ### and two or three rows, depending on whether the 
   ### fit is for a line or a quadratic
   ### Return this list
-  
+  flag = TRUE
+  for(i in 1:B){
+    if(flag){
+      test1 = oneBoot(data,NULL,1)
+      test2 = oneBoot(data,NULL,2)
+      test3 = oneBoot(data,fit1,1)
+      test4 = oneBoot(data,fit2,2)
+      d1 = data.frame(test1)
+      d2 = data.frame(test2)
+      d3 = data.frame(test3)
+      d4 = data.frame(test4)
+      flag = FALSE
+    }else{
+      test1 = oneBoot(data,NULL,1)
+      test2 = oneBoot(data,NULL,2)
+      test3 = oneBoot(data,fit1,1)
+      test4 = oneBoot(data,fit2,2)
+      d1 = data.frame(d1,test1)
+      d2 = data.frame(d2,test2)
+      d3 = data.frame(d3,test3)
+      d4 = data.frame(d4,test4)
+    }
+  }
+  coeff = list(d1,d2,d3,d4)
   return(coeff)
 } 
 
@@ -86,17 +138,28 @@ bootPlot = function(x, y, coeff, trueCoeff){
   ### that generated the data
   
   ### Make a scatter plot of data
-
+  plot(x, y)
   ### Add lines or curves for each row in coeff
   ### Use transparency
   ### You should use mapply to construct all 
   ### 1000 of the bootstrapped lines of best fit 
   ### Have a look at ?mapply for details.
   ### This can be done in ggplot2 or base graphics.
-  
+  for(i in 1:1000){
+    abline(a = as.list(result[[1]][i])[[1]][1], b = as.list(result[[1]][i])[[1]][2],col=rgb(0,0,0,5,maxColorValue=255))
+    abline(a = as.list(result[[3]][i])[[1]][1], b = as.list(result[[3]][i])[[1]][2],col=rgb(0,0,0,5,maxColorValue=255))
+    q = as.list(result[[2]][i])[[1]][1]
+    w = as.list(result[[2]][i])[[1]][2]
+    e = as.list(result[[2]][i])[[1]][3]
+    curve(q*x^2 + w*x + e, add = TRUE,col=rgb(0,0,255,5,maxColorValue=255))
+    q = as.list(result[[4]][i])[[1]][1]
+    w = as.list(result[[4]][i])[[1]][2]
+    e = as.list(result[[4]][i])[[1]][3]
+    curve(q*x^2 + w*x + e, add = TRUE,col=rgb(100,100,100,5,maxColorValue=255))
+  }
   ### Use trueCoeff to add true line/curve - 
   ###  Make the true line/curve stand out
-
+  curve(trueCoeff[1]*x^2 + trueCoeff[2]*x + trueCoeff[3], add = TRUE, col=rgb(255,0,0,maxColorValue=255))
 }
 
 ### Run your simulation by calling this function
