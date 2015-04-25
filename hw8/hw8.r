926ce6b8c82274964a8b793e710d04d05aecc404
GamaWu <spritewu@berkeley.edu>
2015-04-24 23:24:49 -0700

    HW8 Update FINAL VERSION

--- hw8/hw8.r
+++ hw8/hw8.r
@@ -29,18 +29,15 @@ genBootY = function(x, y, rep = TRUE){
   return(result)
 }
 
+
 genBootR = function(fit, err, rep = TRUE){
   ### Sample the errors 
   ### Add the errors to the fit to create a y vector
   ### Return a vector of y values the same length as fit
   ### HINT: It can be easier to sample the indices than the values
-  errors = sample(err, size = length(fit), replace = FALSE)
-  result = c()
-  for(i in 1:length(fit)){
-    result = c(result, fit[i] + errors[i])
-  }
+  indices = sample(1:length(err), length(err), replace=rep)
+  result = fit+err[indices]
   return(result)
- 
 }
 
 fitModel = function(x, y, degree = 1){
@@ -76,16 +73,7 @@ repBoot = function(data, B = 1000){
   
   ### Set up the inputs you need for oneBoot, i.e.,
   ### create errors and fits for line and quadratic
-  x = data[[1]]
-  y = data[[2]]
-  d1model = lm(y ~ x)
-  fitted1 = as.numeric(d1model$fitted.values)
-  err1 = as.numeric(d1model$residuals)
-  fit1 = data.frame(fitted1,err1)
-  d2model = lm(y ~ x + I(x^2))
-  fitted2 = as.numeric(d2model$fitted.values)
-  err2 = as.numeric(d2model$residuals)
-  fit2 = data.frame(fitted2,err2)
+  
   ### replicate a call to oneBoot B times
   ### format the return value so that you have a list of
   ### length 4, one for each set of coefficients
@@ -104,30 +92,46 @@ repBoot = function(data, B = 1000){
   ### and two or three rows, depending on whether the 
   ### fit is for a line or a quadratic
   ### Return this list
-  flag = TRUE
-  for(i in 1:B){
-    if(flag){
-      test1 = oneBoot(data,NULL,1)
-      test2 = oneBoot(data,NULL,2)
-      test3 = oneBoot(data,fit1,1)
-      test4 = oneBoot(data,fit2,2)
-      d1 = data.frame(test1)
-      d2 = data.frame(test2)
-      d3 = data.frame(test3)
-      d4 = data.frame(test4)
-      flag = FALSE
-    }else{
-      test1 = oneBoot(data,NULL,1)
-      test2 = oneBoot(data,NULL,2)
-      test3 = oneBoot(data,fit1,1)
-      test4 = oneBoot(data,fit2,2)
-      d1 = data.frame(d1,test1)
-      d2 = data.frame(d2,test2)
-      d3 = data.frame(d3,test3)
-      d4 = data.frame(d4,test4)
-    }
+  x = data[[1]]
+  y = data[[2]]
+  d1model = lm(y ~ x)
+  fitted1 = as.numeric(d1model$fitted.values)
+  err1 = as.numeric(d1model$residuals)
+  fit1 = data.frame(fitted1,err1)
+  d2model = lm(y ~ x + I(x^2))
+  fitted2 = as.numeric(d2model$fitted.values)
+  err2 = as.numeric(d2model$residuals)
+  fit2 = data.frame(fitted2,err2)
+  
+  c1=c()
+  c2=c()
+  c3=c()
+  c4=c()
+  for (i in 1:B) {
+    temp1= oneBoot(data, NULL, 1)
+    temp2= oneBoot(data, NULL, 2)
+    temp3= oneBoot(data, fit1, 1)
+    temp4= oneBoot(data, fit2, 2)
+    c1 = c(c1,temp1)
+    c2 = c(c2,temp2)
+
   }
-  coeff = list(d1,d2,d3,d4)
+  m1=matrix(c1, 2, B)
+  m2=matrix(c2, 3, B)
+  
+  c3=c()
+  c4=c()
+  for (i in 1:B) {
+    temp3=oneBoot(data,fit1,1)
+    c3=c(c3,temp3)
+    temp4=oneBoot(data,fit2,2)
+    c4=c(c4,temp4)
+  }
+  m3=matrix(c3,2,B)
+  m4=matrix(c4,3,B)
+  
+  coeff=list(m1,m2, m3, m4)
+  
   return(coeff)
 } 
 
@@ -138,28 +142,22 @@ bootPlot = function(x, y, coeff, trueCoeff){
   ### that generated the data
   
   ### Make a scatter plot of data
-  plot(x, y)
+  plot(x,y)
   ### Add lines or curves for each row in coeff
   ### Use transparency
   ### You should use mapply to construct all 
   ### 1000 of the bootstrapped lines of best fit 
   ### Have a look at ?mapply for details.
   ### This can be done in ggplot2 or base graphics.
-  for(i in 1:1000){
-    abline(a = as.list(result[[1]][i])[[1]][1], b = as.list(result[[1]][i])[[1]][2],col=rgb(0,0,0,5,maxColorValue=255))
-    abline(a = as.list(result[[3]][i])[[1]][1], b = as.list(result[[3]][i])[[1]][2],col=rgb(0,0,0,5,maxColorValue=255))
-    q = as.list(result[[2]][i])[[1]][1]
-    w = as.list(result[[2]][i])[[1]][2]
-    e = as.list(result[[2]][i])[[1]][3]
-    curve(q*x^2 + w*x + e, add = TRUE,col=rgb(0,0,255,5,maxColorValue=255))
-    q = as.list(result[[4]][i])[[1]][1]
-    w = as.list(result[[4]][i])[[1]][2]
-    e = as.list(result[[4]][i])[[1]][3]
-    curve(q*x^2 + w*x + e, add = TRUE,col=rgb(100,100,100,5,maxColorValue=255))
-  }
+  
   ### Use trueCoeff to add true line/curve - 
   ###  Make the true line/curve stand out
-  curve(trueCoeff[1]*x^2 + trueCoeff[2]*x + trueCoeff[3], add = TRUE, col=rgb(255,0,0,maxColorValue=255))
+  if (nrow(coeff)==2){
+    sapply(1:ncol(coeff), function(x) abline(coef=coeff[,x],col=rgb(0,0,0,5,maxColorValue=255)))
+  } else {
+    sapply(1:ncol(coeff), function(y) curve(coeff[3,y]*x^2+coeff[2,y]*x+coeff[1,y],col=rgb(0,0,0,5,maxColorValue=255), add=TRUE))
+  }
+  curve(trueCoeff[1] + trueCoeff[2]*x + trueCoeff[3] * x^2, add=TRUE, col='red')
 }
 
 ### Run your simulation by calling this function
@@ -171,8 +169,8 @@ runSim = function() {
   expt = repBoot(data = myData)
   par(mfrow = c(2, 2))
   for (i in 1:4){
-   bootPlot(myData$x, myData$y, 
-            coeff = expt[[i]], trueCoeff) 
+    bootPlot(myData$x, myData$y, 
+             coeff = expt[[i]], trueCoeff) 
   }
   return(expt)
 }
